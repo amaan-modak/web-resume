39c146453266f7abeb3b4ba04d9b74791e4b2080
amaan-modak <amaanakh@buffalo.edu>
2023-08-21 11:16:33 -0400

    Updated sidebar and contact page

--- js/custom.js
+++ js/custom.js
@@ -69,6 +69,9 @@ $(document).ready(function(){
 					case "home" :
 						self.reset();
 						break;
+					case "project" :
+						self.forward($target,$secondary);
+						break;
 					case "p1" :
 						self.forward($target,$secondary);
 						break;
